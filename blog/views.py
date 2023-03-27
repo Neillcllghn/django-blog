86dda6b00210d68b594d39e802d7715be654a953
Neillcllghn <neillcllghn@outlook.com>
2023-03-27 17:49:22 +0000

    Adding the Html templates and creating first Class based view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic  # this is to add generic library
+from .models import Post  # this is to add Post class
 
 # Create your views here.
+
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
